b9f8051972d18ab14a82a31a727c1c58f9a3d5bf
Shubhamprogrammar <mauryashubham12349@gmail.com>
2025-11-23 16:57:52 +0530

    send otp changes

--- models/Conversation.js
+++ models/Conversation.js
@@ -1,8 +1,8 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const conversationSchema = new mongoose.Schema({
   participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   lastMessage: { type: String, default: "" },
 }, { timestamps: true });
 
-export default mongoose.model("Conversation", conversationSchema);
+module.exports =  mongoose.model("Conversation", conversationSchema);
