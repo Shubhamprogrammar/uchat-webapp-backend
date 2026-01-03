be40d8f5fdb41e37a9a24251aead3894052b4ce8
Shubhamprogrammar <mauryashubham12349@gmail.com>
2026-01-04 00:17:00 +0530

    middleware decoding

--- middleware/authoriseuser.js
+++ middleware/authoriseuser.js
@@ -13,8 +13,7 @@ module.exports = function (req, res, next) {
         }
 
         const decoded = jwt.verify(tokenParts[1], process.env.JWT_SECRET);
-
-        req.user = decoded.user;
+        req.user = decoded;
         next();
     } catch (err) {
         return res.status(401).json({ error: "Invalid or expired token" });
